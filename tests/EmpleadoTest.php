b55a04cb39c4b58f00aef1d6970adeee1f53726d
AlejandraAdalid <aleadalid447@gmail.com>
2021-11-22 16:19:43 -0300

    Se agrgeo test get y set

--- tests/EmpleadoTest.php
+++ tests/EmpleadoTest.php
@@ -1,22 +1,42 @@
 <?php
-
- class EmpleadoTest extends \PHPUnit\Framework\TestCase{
-
+abstract class EmpleadoTest extends \PHPUnit\Framework\TestCase{
+	
 	//Funcion crear crea un empleado
-	public function crear ($nombre = "Alejandra", $apellido = "Adalid", $dni = 50123635, $salario = "50000")
+	public function crearDefault($nombre = "Alejandra", $apellido = "Adalid", $dni = "50123635", $salario = "50000")
 	{
 		$empleado = new \App\Empleado ($nombre, $apellido, $dni , $salario);
 		return $empleado;
 	}
 
-	/** @test */
+	//Test para getNombreApellido
 	public function testObtenerNombreApellido()
 	{
-		$empleado = $this-> crear(); 
+		$empleado = $this-> crearDefault(); 
 		$this->assertEquals("Alejandra Adalid", $empleado->getNombreApellido());
 	}
 
-   
+	//Test para getDni
+	public function testFuncionaObtenerDni()
+	{
+		$empleado = $this-> crearDefault(); 
+		$this->assertEquals( 50123635 $empleado->getDni());
+	}
 
+	//Test para getSalario
+	public function testFuncionaObtenerSalario()
+	{
+		$empleado = $this-> crearDefault(); 
+		$this->assertEquals("50000", $empleado->getSalario());
+	} 
 
-}
+	// Test para setSector y getSector
+
+	{
+		$empleado = $this->crearDefault(); 
+		$this->assertEquals("No especificado", $empleado->getSector());
+		
+		$empleado->setSector("Backend"); 
+		$this->assertEquals("Backend", $empleado->getSector());
+	}
+
+	
