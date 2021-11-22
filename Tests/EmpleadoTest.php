627bd5f823184ad5cfaafa7fe940c61fe4837bc2
AlejandraAdalid <aleadalid447@gmail.com>
2021-11-22 16:41:00 -0300

    first commit

---
+++ Tests/EmpleadoTest.php
@@ -0,0 +1,19 @@
+<?php
+abstract class EmpleadoTest extends \PHPUnit\Framework\TestCase{
+	
+	//Funcion crear crea un empleado
+	public function crearDefault($nombre = "Alejandra", $apellido = "Adalid", $dni = "50123635", $salario = "50000")
+	{
+		$empleado = new \App\Empleado ($nombre, $apellido, $dni , $salario);
+		return $empleado;
+	}
+
+	//Test para getNombreApellido
+	public function testObtenerNombreApellido()
+	{
+		$empleado = $this-> crearDefault(); 
+		$this->assertEquals("Alejandra Adalid", $empleado->getNombreApellido());
+	}
+
+
+	}
