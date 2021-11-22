5cb2dd0e0c6f55387fad980605d1f9d99967bde7
AlejandraAdalid <aleadalid447@gmail.com>
2021-11-22 20:10:28 -0300

    los comit faltantes para calcular antiguedad ingresos

--- Tests/EmpleadoPermanenteTest.php
+++ Tests/EmpleadoPermanenteTest.php
@@ -24,7 +24,47 @@ public function testCalcularComisionAntiguedad(){
     $ingreso = new DateTime(); 
     $ingreso->modify('-10 years'); 
     $empleadoPe= $this->crearDefault("Alejandra", "Adalid", 50123635, 50000, $ingreso); 
-    $this->assertEquals("10%",$empleadoPe->calcularComision());
+    $this->assertEquals("15%",$empleadoPe->calcularComision());
+}
+ 
+//Probar que el método calcularIngresoTotal() funciona como se espera.
+
+public function testCalcularIngresoTotal(){
+    $ingreso = new DateTime();
+    $ingreso->modify('-5 years'); 
+    $empleadoPe= $this->crearDefault("Alejandra", "Adalid", 50123635, 50000, $ingreso); 
+    $this->assertEquals(50100,$empleadoPe->calcularIngresoTotal());
 }
 
+
+//Probar que el método calcularAntiguedad() funciona como se espera para un empleado con varios años de antigüedad.
+
+public function testAntiguedad(){
+    $ingreso = new DateTime(); 
+    $ingreso->modify('-10 years'); 
+    $empleadoPe= $this->crearDefault("Alejandra", "Adalid", 50123635, 50000, $ingreso); 
+    $this->assertEquals(5,$empleadoPe->calcularAntiguedad());
+}
+
+//Probar que, si construyo un empleado sin proporcionar la fecha de ingreso, el método getFechaIngreso() retorna la fecha del día, y el método getAntiguedad retorna 0.
+
+public function testFechaSinProporcionar()
+{
+    $empleadoPe = $this->crearDefault("Alejandra", "Adalid", 50123635, 50000, $ingreso);
+    $fecha = new DateTime();
+    $this->assertEquals(date_format($fecha, 'y-m-d'), date_format($empleadoPe->getFechaIngreso(), 'y-m-d')); 
+    $this->assertEquals(0, $empleadoPe->calcularAntiguedad()); 
+
+}
+
+//Probar que, si construyo un empleado proporcionando una fecha de ingreso posterior a la de hoy, lanza una excepción.
+
+   public function testFechaPosterior(){
+    $ingreso = new DateTime(); 
+    $ingreso->modify('+5 years'); 
+    $this->expectException(\Exception::class); 
+    $empleadoPe= $this->crearDefault("Alejandra", "Adalid", 50123635, 50000, $ingreso); 
+
+
+   }
 }
