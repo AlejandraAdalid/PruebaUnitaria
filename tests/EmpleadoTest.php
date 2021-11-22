24a90a7e32edcba3ef409d4af0a5913d2805a736
AlejandraAdalid <aleadalid447@gmail.com>
2021-11-22 16:15:31 -0300

    Se agrgeo test get y set

--- tests/EmpleadoTest.php
+++ tests/EmpleadoTest.php
@@ -1,30 +1,22 @@
 <?php
-abstract class EmpleadoTest extends \PHPUnit\Framework\TestCase{
-	
+
+ class EmpleadoTest extends \PHPUnit\Framework\TestCase{
+
 	//Funcion crear crea un empleado
-	public function crearDefault($nombre = "Alejandra", $apellido = "Adalid", $dni = "50123635", $salario = "50000")
+	public function crear ($nombre = "Alejandra", $apellido = "Adalid", $dni = 50123635, $salario = "50000")
 	{
 		$empleado = new \App\Empleado ($nombre, $apellido, $dni , $salario);
 		return $empleado;
 	}
 
-	//Test para getNombreApellido
+	/** @test */
 	public function testObtenerNombreApellido()
 	{
-		$empleado = $this-> crearDefault(); 
+		$empleado = $this-> crear(); 
 		$this->assertEquals("Alejandra Adalid", $empleado->getNombreApellido());
 	}
 
-	//Test para getDni
-	public function testFuncionaObtenerDni()
-	{
-		$empleado = $this-> crearDefault(); 
-		$this->assertEquals( 50123635 $empleado->getDni());
-	}
+   
 
-	//Test para getSalario
-	public function testFuncionaObtenerSalario()
-	{
-		$empleado = $this-> crearDefault(); 
-		$this->assertEquals("50000", $empleado->getSalario());
-	} 
+
+}
